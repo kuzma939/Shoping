ca0b3e34a3d6201737a0b73c879fac9c4d2e8acb
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2025-01-18 20:30:54 +0100

    Update top-products-jsonld.js

--- src/app/seo/top-products-jsonld.js
+++ src/app/seo/top-products-jsonld.js
@@ -1,20 +1,6 @@
 const topProductsJsonLd = (products) => ({
   "@context": "https://schema.org",
   "@type": "ItemList",
-  availableLanguage: [
-    {
-      "@type": "Language",
-      name: "Ukrainian", // Мова Українська
-    },
-    {
-      "@type": "Language",
-      name: "English", // Мова Англійська
-    },
-    {
-      "@type": "Language",
-      name: "French", // Французька
-    },
-  ],
   itemListElement: products.map((product, index) => ({
     "@type": "ListItem",
     position: index + 1,
@@ -22,7 +8,7 @@ const topProductsJsonLd = (products) => ({
     name: product.translations?.EN?.name || "Unnamed Product", // Назва
     image: product.image || "https://shoping-tdfr.vercel.app/default-image.jpg", // Зображення
     description: product.translations?.EN?.description || "No description available", // Опис
-    dateModified: product.dateModified || new Date().toISOString(), // Дата модифікації
+    
   })),
 });
 
