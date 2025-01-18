883beeecbccb12f3f6b396dce50696af78d18bce
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2025-01-18 20:03:09 +0100

    Update offers-jsonld.js

--- src/app/seo/offers-jsonld.js
+++ src/app/seo/offers-jsonld.js
@@ -1,11 +1,6 @@
 const offersJsonLd = (offers) => ({
   "@context": "https://schema.org",
   "@type": "ItemList",
-  availableLanguage: [
-    { "@type": "Language", name: "Ukrainian" },
-    { "@type": "Language", name: "English" },
-    { "@type": "Language", name: "French" },
-  ],
   itemListElement: offers.map((offer, index) => ({
     "@type": "ListItem",
     position: index + 1,
@@ -13,7 +8,7 @@ const offersJsonLd = (offers) => ({
     name: offer.translations?.EN?.name || "Unnamed Product",
     image: {
       "@type": "ImageObject",
-      url: `https://shoping-tdfr.vercel.app${offer.image}`,
+      url: offer.image ? `https://shoping-tdfr.vercel.app${offer.image}` : "",
       width: 1200,
       height: 628,
       caption: offer.translations?.EN?.name || "Unnamed Product",
@@ -25,11 +20,6 @@ const offersJsonLd = (offers) => ({
       priceCurrency: "UAH",
       availability: "https://schema.org/InStock",
     },
-    sku: offer.sku,
-    color: offer.colors.join(", "),
-    size: offer.sizes.join(", "),
-    category: offer.category,
-    dateModified: new Date().toISOString(), // Актуальна дата
   })),
 });
 
