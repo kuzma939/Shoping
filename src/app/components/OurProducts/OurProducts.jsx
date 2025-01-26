c12994204bf102d3a00dbc475c6f71043aca92c4
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2025-01-26 18:52:07 +0100

    fiks

--- src/app/components/OurProducts/OurProducts.jsx
+++ src/app/components/OurProducts/OurProducts.jsx
@@ -1,32 +1,36 @@
 "use client";
+
 import Image from "next/image";
 import useImageFollow from "../../hooks/useImageFollow";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import Link from "next/link";
-import { useLanguage } from "../../Functions/useLanguage"; 
+import { useLanguage } from "../../Functions/useLanguage";
 import useKeyboardNavigation from "../../hooks/useKeyboardNavigation";
+
 export default function OurProducts() {
-  const {  translateList } = useLanguage(); 
-  
+  const { translateList } = useLanguage();
   const menuItems = translateList("home", "Our_Products");
+
   const images = [
-    { src: "/1.jpg", link: "/All_products" },
-    { src: "/2.jpg", link: "/All_products" },
-    { src: "/3.jpg", link: "/All_products" },
-    { src: "/4.jpg", link: "/All_products" },
-    { src: "/5.jpg", link: "/All_products" },
-    { src: "/6.jpg", link: "/All_products" },
-    { src: "/7.jpg", link: "/All_products" },
-    { src: "/8.jpg", link: "/All_products" },
-    { src: "/9.jpg", link: "/All_products" },
-    { src: "/10.jpg", link: "/All_products" },
+    { src: "/1.jpg", link: "/All-products" },
+    { src: "/2.jpg", link: "/All-products" },
+    { src: "/3.jpg", link: "/All-products" },
+    { src: "/4.jpg", link: "/All-products" },
+    { src: "/5.jpg", link: "/All-products" },
+    { src: "/6.jpg", link: "/All-products" },
+    { src: "/7.jpg", link: "/All-products" },
+    { src: "/8.jpg", link: "/All-products" },
+    { src: "/9.jpg", link: "/All-products" },
+    { src: "/10.jpg", link: "/All-products" },
   ];
 
   const { displayedImages, handleNext, handlePrev } = useImageFollow(
     images.length,
     5
   );
+
   useKeyboardNavigation(handlePrev, handleNext);
+
   return (
     <section
       id="our-products"
@@ -38,39 +42,40 @@ export default function OurProducts() {
         </h2>
         <div className="border-t border-gray-300 dark:border-gray-700"></div>
       </div>
-      <div className="flex items-center justify-center space-x-4  ml-4 mt-8">
+      <div className="flex items-center justify-center space-x-4 mt-8">
+        {/* Ліва кнопка */}
         <div
           onClick={handlePrev}
           className="text-black dark:text-gray-300 text-2xl sm:text-3xl cursor-pointer mx-2 sm:mx-4 hover:text-gray-500 dark:hover:text-gray-400 transition-all duration-300"
         >
           <FaChevronLeft />
         </div>
-        {/* Images */}
-        <div className="flex overflow-x-auto gap-4 sm:gap-6 md:gap-8">
-  {displayedImages.map((imageIndex) => (
-    <Link
-      key={imageIndex}
-      href={images[imageIndex].link}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="flex-shrink-0 group"
-    >
-      <Image
-        src={images[imageIndex].src}
-        alt={`Image ${imageIndex + 1}`}
-        width={150} 
-        height={150}
-        quality={100} 
-        className="rounded-lg object-cover shadow-lg transition-transform duration-500 ease-in-out group-hover:scale-110 group-hover:opacity-90
-          w-[calc(39vw-16px)]
-        sm:w-[150px] sm:h-[150px]
-          md:w-[200px] md:h-[300px]
-          lg:w-[250px] lg:h-[350px]
-          xl:w-[300px] xl:h-[350px]"
-      />
-    </Link>
-  ))}
-</div>
+
+        {/* Карусель */}
+        <div className="flex overflow-x-auto gap-4 w-full px-4 sm:gap-6 md:gap-8">
+          {displayedImages.map((imageIndex) => (
+            <Link
+              key={imageIndex}
+              href={images[imageIndex].link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex-shrink-0 group"
+            >
+              <Image
+                src={images[imageIndex].src}
+                alt={`Топ продукт ${imageIndex + 1}`}
+                width={200} // Зменшений розмір для мобільних
+                height={250} // Пропорційна висота
+                sizes="(max-width: 768px) 45vw, (max-width: 1024px) 20vw, 300px"
+                style={{ width: "auto", height: "auto" }}
+                quality={100}
+                className="rounded-lg object-cover shadow-lg transition-transform duration-500 ease-in-out group-hover:scale-110 group-hover:opacity-90"
+              />
+            </Link>
+          ))}
+        </div>
+
+        {/* Права кнопка */}
         <div
           onClick={handleNext}
           className="text-black dark:text-gray-300 text-2xl sm:text-3xl cursor-pointer mx-2 sm:mx-4 hover:text-gray-500 dark:hover:text-gray-400 transition-all duration-300"
