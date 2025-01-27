f1a6e7acdb30ca6571f9f75756d7adc44b4e0800
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2025-01-27 10:20:42 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,13 @@
 const nextConfig = {
   reactStrictMode: true, // Увімкнення строгого режиму React
   images: {
+    formats: ['image/webp'],
     domains: [
+      
       'shoping-tdfr.vercel.app', // Ваш основний домен
       //'cdn.example.com',         // Приклад CDN, якщо використовується
       //'images.unsplash.com',     // Інший приклад зовнішнього домену
+
     ],
   },
 };
