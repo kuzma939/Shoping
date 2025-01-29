8d88ad264175b1a30aafc9e8e865fdd6ba1c28e2
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2025-01-29 11:33:32 +0100

    Update Hero.jsx

--- src/app/components/Hero/Hero.jsx
+++ src/app/components/Hero/Hero.jsx
@@ -36,7 +36,7 @@ const Hero = () => {
               priority
               style={{ aspectRatio: "1 / 1" }}
             />
-            <div className="w-full flex justify-center">
+            <div className="w-full flex justify-center relative -mt-8">
               <h2 className="text-center text-3xl font-bold text-white bg-black bg-opacity-50 py-2 px-6 rounded-lg">
                 LATORE ATELIER
               </h2>
