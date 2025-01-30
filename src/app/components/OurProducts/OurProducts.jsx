d7c59d2dfea468e644bdbf697a48b0c1b17c1aac
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2025-01-30 10:53:25 +0100

    Update OurProducts.jsx

--- src/app/components/OurProducts/OurProducts.jsx
+++ src/app/components/OurProducts/OurProducts.jsx
@@ -50,12 +50,8 @@ export default function OurProducts() {
         >
           <FaChevronLeft />
         </div>
- {/* Carousel */}
- <div
-          className="flex overflow-x-auto gap-4 w-full px-4 sm:gap-6 md:gap-8"
-          style={{ height: "auto" }} 
-          // Fixed height to prevent layout shifts
-        >
+        {/* Карусель зображень */}
+        <div className="flex overflow-x-auto gap-4 w-full px-4 sm:gap-6 md:gap-8">
           {displayedImages.map((imageIndex) => (
             <Link
               key={imageIndex}
@@ -66,16 +62,18 @@ export default function OurProducts() {
             >
               <Image
                 src={images[imageIndex].src}
-                alt={`Our Product ${imageIndex + 1}`}
-                width={300} // Defined width
-                height={400} // Defined height
-                style={{
-                  aspectRatio: "3/4", // Maintain 3:4 aspect ratio
-                  objectFit: "cover", // Ensure full image visibility
-                }}
-                sizes="(max-width: 768px) 45vw, (max-width: 1024px) 20vw, 300px"
-                quality={100}
-                className="rounded-lg object-cover shadow-lg transition-transform duration-500 ease-in-out group-hover:scale-110 group-hover:opacity-90"
+                alt={`Зображення ${imageIndex + 1}`}
+                width={200} // Ширина для мобільних пристроїв
+                height={250} // Пропорційна висота
+                sizes="(max-width: 768px) 45vw, (max-width: 1024px) 20vw, 300px" // Аналогічно TopProducts
+                style={{ width: "auto", height: "auto" }}
+                quality={100} // Висока якість
+                className="rounded-lg object-cover shadow-lg transition-transform duration-500 ease-in-out group-hover:scale-110 group-hover:opacity-90
+                  w-[calc(39vw-16px)]
+                  sm:w-[150px] sm:h-[150px]
+                  md:w-[200px] md:h-[300px]
+                  lg:w-[250px] lg:h-[350px]
+                  xl:w-[300px] xl:h-[350px]"
               />
             </Link>
           ))}
