b7da84e5417d658005c023ae664c4b0abb50c6ec
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2025-01-31 15:13:08 +0100

    Update useViewportSizeHero.js

--- src/app/hooks/useViewportSizeHero.js
+++ src/app/hooks/useViewportSizeHero.js
@@ -1,5 +1,32 @@
 import { useState, useEffect } from "react";
 
+export function useViewportSize(defaultViewport = "desktop") {
+  const [viewportSize, setViewportSize] = useState(defaultViewport);
+
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+
+    const getViewport = () => {
+      if (window.innerWidth <= 450) return "mobile";
+      if (window.innerWidth <= 1024) return "tablet";
+      return "desktop";
+    };
+
+    const handleResize = () => {
+      setViewportSize(getViewport());
+    };
+
+    setViewportSize(getViewport()); // Визначення розміру при першому рендерингу
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return viewportSize;
+}
+{/*
+import { useState, useEffect } from "react";
+
 export function useViewportSize() {
   const [viewportSize, setViewportSize] = useState("desktop"); // За замовчуванням для SSR
 
